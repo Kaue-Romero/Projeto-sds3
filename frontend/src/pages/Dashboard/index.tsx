0b5f84eeec91323b3ae1fe65bf16b31a424af1a1
Kaue-Romero <kaueleiving@gmail.com>
2021-05-08 15:29:10 -0300

    Home, navigation

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -10,7 +10,6 @@ function Dashboard() {
             <NavBar />
             <div className="container">
                 <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-
                 <div className="row px-3">
                     <div className="col-sm-6">
                         <h5 className="text-center text-secondary">Sucesso nas vendas {'('}%{')'}</h5>
